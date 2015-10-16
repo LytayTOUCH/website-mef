8ceb376eb12a10e9d2d1b90bbeb1334c78c64453
kung phanith <kungphanith79@gmail.com>
2015-10-16 17:16:12 +0700

    change hierachy and change map to set lat and lng as center poin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,53 +24,7 @@
 
 //= require owl.carousel
 
-
-// This Javascript is for datetime.picker
-$(document).on('ready page:change', function() {
-  $('.is-data-table').DataTable();
-
-  $('.datetimepicker').datetimepicker({
-    // put here your custom picker options, that should be applied for all pickers
-  });
-
-  $('.datetimerange').each(function(){
-    var $this = $(this)
-    var range1 = $($this.find('.input-group')[0])
-    var range2 = $($this.find('.input-group')[1])
-
-    if(range1.data("DateTimePicker").date() != null)
-      range2.data("DateTimePicker").minDate(range1.data("DateTimePicker").date());
-
-    if(range2.data("DateTimePicker").date() != null)
-      range1.data("DateTimePicker").maxDate(range2.data("DateTimePicker").date());
-
-    range1.on("dp.change",function (e) {
-      if(e.date)
-        range2.data("DateTimePicker").minDate(e.date);
-      else
-        range2.data("DateTimePicker").minDate(false);
-    });
-
-    range2.on("dp.change",function (e) {
-      if(e.date)
-        range1.data("DateTimePicker").maxDate(e.date);
-      else
-        range1.data("DateTimePicker").maxDate(false);
-    });
-  });
+//= require custom_script
 
 
-  $("#slideshow1").owlCarousel({
-    autoPlay : 3000,
-    navigation : false, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem:true,
-    stopOnHover : true,
-
-    autoHeight : true
-  });
-
-
-});
 
